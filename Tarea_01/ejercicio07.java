f1fefb35ba39cfd0314ab301951b3e35f49db2e2
neill04 <141801169+neill04@users.noreply.github.com>
2024-05-09 16:41:30 -0500

    Create ejercicio07.java

---
+++ Tarea_01/ejercicio07.java
@@ -0,0 +1,9 @@
+public class MostrarNumeroPi {
+    public static void main(String[] args) {
+        // Calcular el número Pi utilizando la constante Math.PI de Java
+        double numeroPi = Math.PI;
+
+        // Mostrar el número Pi
+        System.out.println("El número Pi es: " + numeroPi);
+    }
+}
